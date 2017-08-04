235b90537941765c2a74dcb7fc1de3db596f9abc
zzddmm <zzddmm@foxmail.com>
2017-08-04 14:46:34 +0800

    reflact

---
+++ src/reflact/demo1/hello.java
@@ -0,0 +1,12 @@
+package reflact.demo1;
+
+public class hello {
+	
+	public static void main(String[] args) {
+        Demo demo=new Demo();
+        System.out.println(demo.getClass().getName());
+    }
+}
+class Demo{
+    //other codes...
+}
